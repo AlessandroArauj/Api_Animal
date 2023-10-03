1b8f9e9df6f57f22df05d25662da39d86c7e6047
Alezin <araujobr1903@gmail.com>
2023-10-03 16:28:14 -0300

    front feito

--- site/src/App.js
+++ site/src/App.js
@@ -1,12 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        
-      </header>
+      <header></header>
+
+      <section className='f1'>
+
+
+        <h1>Editar ou inserir</h1>
+        <div className='line' />
+        <div className='card'>
+
+          <div>
+            <div>
+              <p>Espécie</p>
+              <input type='text' />
+
+              <p>Genêro</p>
+              <select></select>
+            </div>
+
+            <div>
+              <p>Idade</p>
+              <input type='text' />
+
+              <p>Dieta Alimentar</p>
+              <select></select>
+            </div>
+          </div>
+
+          <button></button>
+
+        </div>
+      </section>
+
+
+      <seciton className='f2'>
+        <h1> Consultar os Animais</h1>
+
+        <label>
+          <input type='text' />
+          <img src='../assets/images/lupa.svg' />
+        </label>
+
+        <div className='line2' />
+
+      </seciton>
     </div>
   );
 }
